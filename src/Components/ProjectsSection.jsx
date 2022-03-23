f23e1804e290c5c32e3b3425bead6de8c8d8fb88
deep1296 <96063026+deep1296@users.noreply.github.com>
2022-03-23 13:17:39 +0530

    test

--- src/Components/ProjectsSection.jsx
+++ src/Components/ProjectsSection.jsx
@@ -16,7 +16,7 @@ const projects = [
     link: "https://gymshark-team-shark.vercel.app/",
     git: "https://github.com/deep1296/Gymshark_FRONTEND_Unit4.git",
     about:
-      "“An e-commerce website of fitness apparel and accessories. Implemented features to get data from backend.",
+      "“An e-commerce website of fitness apparel and accessories. Implemented features to get data from backend,Created routes and Methods,Implemented Dynamic cart functionality",
     stacks: [
       <SiNodedotjs className={styles.stackIcon} />,
       <SiExpress className={styles.stackIcon} />,
@@ -32,27 +32,27 @@ const projects = [
     link: "https://focused-heisenberg-fa48d3.netlify.app/",
     git: "https://github.com/deep1296/cloneOfRodan-Fields.git",
     about:
-      "An e-commerce website of beauty products and therapy. Created in 5 days using HTML,CSS,JavaScript",
+      "An e-commerce website of beauty products and therapy. Created Dynamic cart and payment page using HTML,CSS,JavaScript,Added sorting Fuctionality",
     stacks: [
       <SiJavascript className={styles.stackIcon} />,
       <SiHtml5 className={styles.stackIcon} />,
       <SiCss3 className={styles.stackIcon} />,
     ],
   },
-  // {
-  //   name: "Zara.com clone",
-  //   img: "/zara.jpg",
-  //   link: "https://thotapavankumar.github.io/Zara_Project_Clone/",
-  //   git: "https://github.com/Gajanan-Watt/zara_web.git",
-  //   about:
-  //     " It is an e-commerce website having varieties of luxury items which are of high quality. We have tried to clone the pixel perfect website with the help of HTML. Implemented using HTML, CSS and javascript.",
-  //   stacks: [
-  //     // <SiReact className={styles.stackIcon} />,
-  //     <SiJavascript className={styles.stackIcon} />,
-  //     <SiHtml5 className={styles.stackIcon} />,
-  //     <SiCss3 className={styles.stackIcon} />,
-  //   ],
-  // },
+  {
+    name: "Zara.com clone",
+    img: "/zara.jpg",
+    link: "https://thotapavankumar.github.io/Zara_Project_Clone/",
+    git: "https://github.com/Gajanan-Watt/zara_web.git",
+    about:
+      " It is an e-commerce website having varieties of luxury items which are of high quality. We have tried to clone the pixel perfect website with the help of HTML. Implemented using HTML, CSS and javascript.",
+    stacks: [
+      // <SiReact className={styles.stackIcon} />,
+      <SiJavascript className={styles.stackIcon} />,
+      <SiHtml5 className={styles.stackIcon} />,
+      <SiCss3 className={styles.stackIcon} />,
+    ],
+  },
   // {
   //   name: "Pixel Art",
   //   img: "/doggo2.png",
